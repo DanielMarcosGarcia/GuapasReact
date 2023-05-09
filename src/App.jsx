69d7cd0f5660f06c4b45b22ee9f3fb6235f394a0
Daniel Garcia <danielgarciamarco17@gmail.com>
2023-05-09 20:40:44 -0300

    iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Curso <b>React Js</b> CoderHouse by Daniel Garcia
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Primera Clase
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
